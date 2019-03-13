7647a273ab8534c124f2787e0b58d345d6120994
Gabriel Leventhal <gabriel@leventhal.ch>
2019-03-13 10:08:58 -0400

    salmon 0.14

--- Formula/salmon.rb
+++ Formula/salmon.rb
@@ -2,8 +2,8 @@ class Salmon < Formula
   # cite Patro_2017: "https://doi.org/10.1038/nmeth.4197"
   desc "Transcript-level quantification from RNA-seq reads"
   homepage "https://github.com/COMBINE-lab/salmon"
-  url "https://github.com/COMBINE-lab/salmon/archive/v0.9.1.tar.gz"
-  sha256 "3a32c28d217f8f0af411c77c04144b1fa4e6fd3c2f676661cc875123e4f53520"
+  url "https://github.com/COMBINE-lab/salmon/archive/v0.14.0.tar.gz"
+  sha256 "4a9db47d2270aaba227602fae2ce414122db783200023cf46efe7644846b0d83"
   head "https://github.com/COMBINE-lab/salmon.git"
 
   bottle do
@@ -15,10 +15,15 @@ class Salmon < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "cmake" => :build
   depends_on "boost"
+  depends_on "cmake" => :build
+  depends_on "cereal" => :build
+  depends_on "jemalloc"
+  depends_on "libdivsufsort"
+  depends_on "staden-io-lib"
   depends_on "tbb"
   depends_on "xz"
+
   unless OS.mac?
     depends_on "unzip" => :build
     depends_on "zlib"
@@ -26,9 +31,18 @@ class Salmon < Formula
 
   def install
     # Reduce memory usage for CircleCI.
-    ENV["MAKEFLAGS"] = "-j4" if ENV["CIRCLECI"]
+    ENV["MAKEFLAGS"] = "-j1" if ENV["CIRCLECI"]
 
-    system "cmake", ".", *std_cmake_args
+    if OS.mac?
+      inreplace "include/spdlog/fmt/bundled/format.h", 
+                "FMT_HAS_INCLUDE(<string_view>) && __cplusplus > 201402L", 
+                "FMT_HAS_INCLUDE(<string_view>)"
+      inreplace "include/stx/string_view.hpp",
+                "__has_include(<string_view>) && (__cplusplus > 201402)",
+                "__has_include(<string_view>)"
+    end
+
+    system "cmake", ".", "-DUSE_SHARED_LIBS=1", *std_cmake_args
     system "make"
     system "make", "install"
   end
